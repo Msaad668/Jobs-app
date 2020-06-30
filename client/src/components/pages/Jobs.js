7d705a988af1897824a5884d7167de409f4a8a90
mohammad saad <mostafasaad55@gmail.com>
2020-06-30 20:46:53 +0200

    initially trying the jobs route with the react frontend with axios

--- client/src/components/pages/Jobs.js
+++ client/src/components/pages/Jobs.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Jobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/jobs").then((res) => {
+      setJobs(res.data);
+    });
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div className="container">
       <h2>Explore New Career Opportunities..</h2>
@@ -38,7 +49,29 @@ const Jobs = () => {
         </div>
       </div>
 
-      <div class="card">
+      {jobs.map((job) => {
+        return (
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{job.title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">die freiheit firma</h6>
+              <p class="card-text">{job.description}</p>
+              <button
+                style={{ marginRight: "1rem" }}
+                type="button"
+                class="btn btn-outline-secondary btn-lg"
+              >
+                apply
+              </button>
+              <a href="#" class="btn btn-primary">
+                apply
+              </a>
+            </div>
+          </div>
+        );
+      })}
+
+      {/* <div class="card">
         <h3 class="card-header">Featured</h3>
         <div class="card-body">
           <h5 class="card-title">Special title treatment</h5>
@@ -50,7 +83,7 @@ const Jobs = () => {
             Go somewhere
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
