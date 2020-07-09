8ddcb888d103277f5d60a587416f10fd53ad5a75
mohammad saad <mostafasaad55@gmail.com>
2020-07-09 18:47:53 +0200

    trying

---
+++ client/build/precache-manifest.7c67e23474bf7c0a3bd57212e26e0fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea19e948d0726a127ec6e26de0859c74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9a503196d21b03ae9a",
+    "url": "/static/css/main.6312d65f.chunk.css"
+  },
+  {
+    "revision": "593d0620fd0033925e34",
+    "url": "/static/js/2.724bc153.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.724bc153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9a503196d21b03ae9a",
+    "url": "/static/js/main.2da9f9d9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
